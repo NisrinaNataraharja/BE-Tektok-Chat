3e725d94515c082143b8bc5bde663b0b1d375662
NisrinaNataraharja <nisrinanataraharja@gmail.com>
2022-06-20 22:30:08 +0700

    update

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,13 +2,15 @@ const express = require('express')
 const router = express.Router()
 const productController = require('../controller/products')
 const { protect } = require('../middlewares/auth')
-const middUpload = require('../middlewares/upload')
+// const middUpload = require('../middlewares/upload')
 
 
 router
-  .get('/', productController.selectProductsRecipeWithCondition)
-  .post('/', protect, middUpload("image"), productController.insertRecipe)
-  .put('/:id', protect, middUpload("image"), productController.updateRecipe)
+  .get('/', productController.selectRecipeWithCondition)
+  .post('/', protect, productController.insertRecipe)
+  .put('/:id', protect, productController.updateRecipe)
   .delete('/:id', protect, productController.deleteRecipe)
 
 module.exports = router
+
+// middUpload("image")
